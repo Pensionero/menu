36fdd4aa2dd99f7e4d2e86c481ad2a432f4d544f
Pensionero <Pems.67@yandex.ru>
2020-06-16 23:02:33 +0300

    second commit

--- js/menu.js
+++ js/menu.js
@@ -3,4 +3,19 @@ $(document).ready(function() {
 		$('.header__burger,.header__menu').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
+
+	$(function () {
+		
+		var location = window.location.href;
+		var cur_url = '#site/' + location.split('/').pop();
+		
+		$('.header__list li').each(function () {
+			 var link = $(this).find('a').attr('href');
+			 
+			 if (cur_url == link) {
+				  $(this).addClass('current');
+			 }
+		});
+  });
+
 });
